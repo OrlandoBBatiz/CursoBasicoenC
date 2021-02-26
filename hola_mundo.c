25763c37e8a2ac74491b6b439224123ef1f2875e
OrlandoBBatiz <bguzmans1200@alumno.ipn.mx>
2021-02-25 21:51:49 -0600

    Mi primer commit en Hola_mundo.c para mis panas de YouTube

---
+++ hola_mundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hola panas del Stream, saludos a Pio, Abel, Dene, Yess, Marcos\n\n");
+    printf("Hola chicos del Stream, Danna, Javier Saveedra, Fabian\n");
+
+    return 0;
+}
