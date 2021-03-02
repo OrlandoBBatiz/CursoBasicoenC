378f934d718c10012384ec109c398cd6301c3001
OrlandoBBatiz <bguzmans1200@alumno.ipn.mx>
2021-03-01 20:50:33 -0600

    Agregado la solicitud de variable float con decimal y su impresion

--- ImpresionDatos.c
+++ ImpresionDatos.c
@@ -6,12 +6,18 @@
 int main (){
 
     int variable1 = 0;
+    float variable2 =0;
     char miprimercaracter;
 
     printf("Usuario porfavor ingrese un numero: ");
     scanf("%d",&variable1);
 
     printf("Lo que ingresaste fue: %d\n",variable1);
+
+    printf("Usuario porfavor ingrese un numero con punto decimal: ");
+    scanf("%f",&variable2);
+
+    printf("Lo que ingresaste fue: %f\n",variable2);
 
     printf("Ingresa un caracter porfavor: ");
     fflush(stdin);
