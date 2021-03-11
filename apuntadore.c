020f1e338bdb86ab361e830a7c216539a8086b5c
OrlandoBBatiz <bguzmans1200@alumno.ipn.mx>
2021-03-10 20:35:29 -0600

    Ejemplo mas pro y basico de uso de Apuntadores, solo para practicar con los panas y las panas del Stream

--- apuntadore.c
+++ apuntadore.c
@@ -7,13 +7,33 @@ int main(){
     int mes=0;
     int anio=0;
 
-    int *apDia;
-    apDia = &dia;
+    int *apFecha;
+    
 
-    printf("Dame el dia: ");
-    scanf("%d",apDia);
+    printf("Ingrese el dia: ");
+    scanf("%d",&dia);
+    printf("Ingrese el mes: ");
+    scanf("%d",&mes);
+    printf("Ingrese el anio: ");
+    scanf("%d",&anio);
 
-    printf("El dias es: %d, guardado en: %X o %p",*apDia,apDia,apDia);
+    printf("La fecha de hoy es: %d / %d / %d\n\n",dia,mes,anio);
+
+    printf("Perdi la informacion, porfavor ingrese de nuevo los datos:\n\n");
+
+    apFecha = &dia;
+    printf("Ingrese el dia: ");
+    scanf("%d",apFecha);
+
+    apFecha = &mes;
+    printf("Ingrese el mes: ");
+    scanf("%d",apFecha);
+
+    apFecha = &anio;
+    printf("Ingrese el anio: ");
+    scanf("%d",apFecha);
+
+    printf("La fecha de hoy es: %d / %d / %d\n\n",dia,mes,*apFecha);
 
     return 0;
 }
