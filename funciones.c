23751aef77a173cd41aa9bc806d86c02088340eb
OrlandoBBatiz <bguzmans1200@alumno.ipn.mx>
2021-03-12 21:33:04 -0600

    Funciones de paso de parametro por Referencia usando estructuras de Datos

--- funciones.c
+++ funciones.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void calculaAhorro(int salario, float *ahorro);
-void calculaAniosRetiro(int salario, int edad, float *ahorro, float *anioR);
-void imprimirResultados(char *empresa, char *nombre_T, int *edad, int *salario, float *ahorro, float *anioR);
-
-int main(){
+typedef struct Trabajador{
 
     char empresa[30];
     char nombre_T[40];
-    int edad = 0;
-    int salario = 0;
-    int opc = 0;
-    float ahorro = 0;
-    float anioR = 0;
+    int edad;
+    int salario;
+    float ahorro;
+    float anioR;
 
+}Trabajador;
+
+void capturarDatos(Trabajador *empleado);
+void calculaAhorro(Trabajador *empleado);
+void calculaAniosRetiro(Trabajador *empleado);
+void imprimirResultados(Trabajador *empleado);
+
+int main(){
+
+    Trabajador empleado;
+    int opc=0;
 
     do{
-        printf("====================== CAPTURADOR DE DATOS ======================\n\n");
-        printf("Nombre de la Empresa: ");
-        fflush(stdin);
-        fgets(empresa,sizeof(empresa),stdin);
-        printf("Nombre del Trabajado: ");
-        fflush(stdin);
-        fgets(nombre_T,sizeof(nombre_T),stdin);
-        printf("Edad: ");
-        scanf("%d",&edad);
-        printf("Salario: ");
-        scanf("%d",&salario);
-
-        calculaAhorro(salario,&ahorro);
-        calculaAniosRetiro(salario,edad,&ahorro,&anioR);
-        imprimirResultados(empresa,nombre_T,&edad,&salario,&ahorro,&anioR);
+        
+        capturarDatos(&empleado);
+        calculaAhorro(&empleado);
+        calculaAniosRetiro(&empleado);
+        imprimirResultados(&empleado);
 
 
         printf("Quieres ingresar otro Trabajador:\n\n");
@@ -42,37 +38,52 @@ int main(){
     }while(opc != 2);
        
 }
+void capturarDatos(Trabajador *empleado)
+{
+    printf("====================== CAPTURADOR DE DATOS ======================\n\n");
+    printf("Nombre de la Empresa: ");
+    fflush(stdin);
+    fgets(empleado->empresa,sizeof(empleado->empresa),stdin);
+    printf("Nombre del Trabajado: ");
+    fflush(stdin);
+    fgets(empleado->nombre_T,sizeof(empleado->nombre_T),stdin);
+    printf("Edad: ");
+    scanf("%d",&empleado->edad);
+    printf("Salario: ");
+    scanf("%d",&empleado->salario);
+};
 
-void calculaAhorro(int salario, float *ahorro)
+void calculaAhorro(Trabajador *empleado)
 {
     //Parametros en Funcion = Parametros mandados por la funcion donde fue llamado
     //int salario = salario;
     //float *ahorro = &salario;
     float dummyAhorro = 0;
-    dummyAhorro = (float)salario * 0.1;
-    *ahorro = dummyAhorro;
+    dummyAhorro = (float)empleado->salario * 0.1;
+    empleado->ahorro = dummyAhorro;
+    //printf("Ahorro %f\n",empleado->ahorro);
     //printf("Termine calculo de Ahorro\n");
 };
-void calculaAniosRetiro(int salario, int edad, float *ahorro, float *anioR)
+void calculaAniosRetiro(Trabajador *empleado)
 {
     float suma = 1000000;
-    float dummyAhorro = *ahorro;
+    float dummyAhorro = empleado->ahorro;
     float dummyanioR =0;
 
     dummyAhorro = dummyAhorro*12;
     dummyanioR = suma / dummyAhorro;
 
-    *anioR = edad + dummyanioR;
+    empleado->anioR = empleado->edad + dummyanioR;
     //printf("Termine calculo de Anios\n");
 
 };
-void imprimirResultados(char *empresa, char *nombre_T, int *edad, int *salario, float *ahorro, float *anioR)
+void imprimirResultados(Trabajador *empleado)
 {
     printf("==================== RESULTADOS FINANCIEROS ====================\n\n");
-    printf("Empresa en la que trabaja: %s\n", empresa);
-    printf("Nombre del trabajador: %s\n", nombre_T);
-    printf("Edad del trabajador: %d\n",*edad);
-    printf("Salario es: $ %d\n",*salario);
-    printf("Ahorro Mensual Calculado: $ %f\n",*ahorro);
-    printf("Anios que tendra el Trabajador al Retirarse: %f\n",*anioR);
+    printf("Empresa en la que trabaja: %s\n", empleado->empresa);
+    printf("Nombre del trabajador: %s\n", empleado->nombre_T);
+    printf("Edad del trabajador: %d\n",empleado->edad);
+    printf("Salario es: $ %d\n",empleado->salario);
+    printf("Ahorro Mensual Calculado: $ %f\n",empleado->ahorro);
+    printf("Anios que tendra el Trabajador al Retirarse: %f\n",empleado->anioR);
 };
